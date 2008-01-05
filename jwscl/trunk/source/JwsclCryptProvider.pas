4aca815829e62917d728e1858b9886535a5f55bb
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2008-01-05 17:49:54 +0000

    +added GPL as second license - this was defined already but not mentioned in the docs +updated help -removed absolute paths from TSAdminEx.cfg
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@157 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

--- jwscl/trunk/source/JwsclCryptProvider.pas
+++ jwscl/trunk/source/JwsclCryptProvider.pas
@@ -15,6 +15,18 @@ Software distributed under the License is distributed on an "AS IS" basis, WITHO
 ANY KIND, either express or implied. See the License for the specific language governing rights
 and limitations under the License.
 
+Alternatively, the contents of this file may be used under the terms of the  
+GNU Lesser General Public License (the  "LGPL License"), in which case the   
+provisions of the LGPL License are applicable instead of those above.        
+If you wish to allow use of your version of this file only under the terms   
+of the LGPL License and not to allow others to use your version of this file 
+under the MPL, indicate your decision by deleting  the provisions above and  
+replace  them with the notice and other provisions required by the LGPL      
+License.  If you do not delete the provisions above, a recipient may use     
+your version of this file under either the MPL or the LGPL License.          
+                                                                             
+For more information about the LGPL: http://www.gnu.org/copyleft/lesser.html 
+
 The Initial Developer of the Original Code is Philip Dittmann.
 Portions created by Philip Dittmann are Copyright (C) Philip Dittmann. All rights reserved.
 
