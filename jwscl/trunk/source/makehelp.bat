f832e87801d6b800011ef0defd4dc84c92acda61
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2008-02-11 18:19:05 +0000

    added help css
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@266 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

--- jwscl/trunk/source/makehelp.bat
+++ jwscl/trunk/source/makehelp.bat
@@ -6,4 +6,9 @@ REM call this script with "-O htmlhelp" to create HTML Help project files
 
 mkdir pasdoc
 pasdoc %1 %2 -N "JWSCL Help" -T "JEDI Windows Security Code Library" --write-uses-list --introduction "..\documentation\introduction.txt" --conclusion "..\documentation\conclusion.txt" --auto-link -E .\pasdoc *.pas
+
+REM overwrite default style file with the one of JWSCL
+copy /Y "JWSCLHelpBlue.css" .\pasdoc\pasdoc.css
+
 pause
+
