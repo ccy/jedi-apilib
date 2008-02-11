36b1836e8fbe780cef2934ff66f31531419529ba
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2008-02-11 20:24:28 +0000

    added jwaVista.pas to help compile
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@269 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

--- jwscl/trunk/source/makehelp.bat
+++ jwscl/trunk/source/makehelp.bat
@@ -5,7 +5,9 @@ REM
 REM call this script with "-O htmlhelp" to create HTML Help project files
 
 mkdir pasdoc
-pasdoc %1 %2 -N "JWSCL Help" -T "JEDI Windows Security Code Library" --write-uses-list --introduction "..\documentation\introduction.txt" --conclusion "..\documentation\conclusion.txt" --auto-link -E .\pasdoc *.pas
+REM pasdoc %1 %2 -N "JWSCL Help" -T "JEDI Windows Security Code Library" --write-uses-list --introduction "..\documentation\introduction.txt" --conclusion "..\documentation\conclusion.txt" --auto-link -D PACKAGE_CONDITIONS;JWA_INCLUDE_SETUP_API;JWA_INCLUDE_JWAADSTLB;DYNAMIC_LINK -I ..\..\..\jwapi\trunk\Common -E .\pasdoc *.pas ..\..\..\jwapi\trunk\Win32API\JwaWindows.pas ..\..\..\jwapi\trunk\Win32API\JwaVista.pas 
+REM pasdoc %1 %2 -N "JWSCL Help" -T "JEDI Windows Security Code Library" --write-uses-list --introduction "..\documentation\introduction.txt" --conclusion "..\documentation\conclusion.txt" --auto-link -D PACKAGE_CONDITIONS;JWA_INCLUDE_SETUP_API;JWA_INCLUDE_JWAADSTLB;DYNAMIC_LINK -I ..\..\..\jwapi\trunk\Common;..\..\..\jwapi\trunk\Win32API\ -E .\pasdoc ..\..\..\jwapi\trunk\Win32API\JwaWindows.pas
+pasdoc %1 %2 -N "JWSCL Help" -T "JEDI Windows Security Code Library" --write-uses-list --introduction "..\documentation\introduction.txt" --conclusion "..\documentation\conclusion.txt" --auto-link -D PACKAGE_CONDITIONS;JWA_INCLUDE_SETUP_API;JWA_INCLUDE_JWAADSTLB;DYNAMIC_LINK -I ..\..\..\jwapi\trunk\Common -E .\pasdoc *.pas "..\..\..\jwapi\trunk\Win32API\JwaVista.pas"
 
 REM overwrite default style file with the one of JWSCL
 copy /Y "JWSCLHelpBlue.css" .\pasdoc\pasdoc.css
