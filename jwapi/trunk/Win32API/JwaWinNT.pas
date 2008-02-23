2c442310f6ac3f7d94d945d8402fa09039e26304
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2008-02-23 23:54:04 +0000

    +This fix was provided by a member of the jedi.apiconversion mailinglist : For example the call Int32x32To64(2081, 10000000) correctly returns 20810000000 in VC++, but with the current implementation of the API-conversion it returns -664836480 in Delphi. With the fix below (added Int64-casts) it works as expected also in Delphi: +Precaution: Did the same thing for UInt32x32To64
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@314 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

--- jwapi/trunk/Win32API/JwaWinNT.pas
+++ jwapi/trunk/Win32API/JwaWinNT.pas
@@ -9047,12 +9047,12 @@ end;
 
 function Int32x32To64(a, b: LONG): LONGLONG;
 begin
-  Result := a * b;
+  Result := Int64(a) * Int64(b);
 end;
 
 function UInt32x32To64(a, b: DWORD): ULONGLONG;
 begin
-  Result := a * b;
+  Result := ULONGLONG(a) * ULONGLONG(b);
 end;
 
 {$ENDIF JWA_INCLUDEMODE}
