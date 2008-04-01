52b3fce4409e44b7c9be3b48168a009d649f7dce
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2008-04-01 14:14:48 +0000

    +Added RunAsSys - a program to run processes as system user
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@354 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

---
+++ jwscl/trunk/examples/RunAsSys/RunAsSys.dpr
@@ -0,0 +1,141 @@
+program RunAsSys;
+
+uses
+  SvcMgr,
+  SysUtils,
+  Dialogs,
+  JwaWindows,
+  JwsclToken,
+  JwsclKnownSid,
+  JwsclElevation,
+  JwsclVersion,
+  JwsclExceptions,
+  RunAsSysService in 'RunAsSysService.pas' {RunAsSysSvc3: TService};
+
+{$R *.RES}
+
+
+procedure StartTheService(Service : TService);
+var
+  hSCM: SC_HANDLE;
+  hSvc: SC_HANDLE;
+  args: array of PChar;
+  i : Integer;
+begin
+  hSCM := OpenSCManager(nil, SERVICES_ACTIVE_DATABASEW, SC_MANAGER_CONNECT);
+  if hSCM = 0 then
+    RaiseLastOSError;
+
+  try
+    hSvc := OpenService(hSCM, PChar(Service.Name), SERVICE_START);
+    if hSvc = 0 then
+      RaiseLastOSError;
+
+    try
+      SetLength(args, ParamCount+1);
+
+      args[0] := PChar(IntToStr(GetProcessLogonSession));
+      for i := 1 to ParamCount do
+      begin
+        GetMem(args[i], Length(ParamStr(i))+2);
+        StringCchCopy(args[i], Length(ParamStr(i))+2, PChar(ParamStr(i)));
+      end;
+      try
+        if not StartServiceA(hSvc, Length(args), @args[0]) then
+          RaiseLastOSError;
+      finally
+        for i := 1 to ParamCount do
+        begin
+          FreeMem(args[i]);
+        end;
+      end;
+    finally
+      CloseServiceHandle(hSvc);
+    end;
+  finally
+   CloseServiceHandle(hScm);
+  end;
+end;
+
+var i : Integer;
+   Parameters : String;
+begin
+  JwInitWellKnownSIDs;
+  if JwIsPrivilegeSet(SE_TCB_NAME, pqt_Available) and
+   {  JwIsPrivilegeSet(SE_CREATE_TOKEN_NAME, pqt_Available) and}
+     (JwSecurityProcessUserSID.EqualSid(JwLocalSystemSID)) then
+  begin
+    //Sleep(10000);
+    SvcMgr.Application.Initialize;
+    SvcMgr.Application.CreateForm(TRunAsSysSvc3, RunAsSysSvc3);
+{$IFDEF CMD}
+    RunAsSysSvc.DoExecute();
+{$ELSE}
+    SvcMgr.Application.Run;
+{$ENDIF}
+  end
+  else
+  if (JwCheckAdministratorAccess) then
+  begin
+    //ShowMessage(CmdLine);
+
+    SvcMgr.Application.Free;
+    SvcMgr.Application := TServiceApplicationEx.Create(nil);
+    SvcMgr.Application.Initialize;
+    SvcMgr.Application.CreateForm(TRunAsSysSvc3, RunAsSysSvc3);
+
+    try
+      TServiceApplicationEx(SvcMgr.Application).RegisterServices(true, true);
+    except
+      on E : EOSError do
+      begin
+        if E.ErrorCode <> 1073 then
+          raise;
+      end;
+    end;
+    try
+      StartTheService(RunAsSysSvc3);
+    finally
+     TServiceApplicationEx(SvcMgr.Application).RegisterServices(false, true);
+    end;
+    //FreeAndNil(SvcMgr.Application);
+    SvcMgr.Application.Run;
+  end
+  else
+  begin
+    try
+      if not (TJwWindowsVersion.IsWindowsVista(true) or
+        TJwWindowsVersion.IsWindows2008(true)) then
+      begin
+        ShowMessage('You have to logon as administrator. Please enter your admin credentials.');
+      end;
+
+      Parameters := '';
+      for i := 1 to ParamCount do
+      begin
+        Parameters := Parameters + ' ' +ParamStr(i);
+      end;
+
+      JwShellExecute(0, //const hWnd: HWND;
+        ParamStr(0), //FileName,
+        Parameters, //Parameters,
+        '', //Directory: TJwString;
+        0, //ShowCmd: Integer;
+        [sefIgnoreElevationIfNotAvailable]//Flags : TJwShellExecuteFlags = [sefNoClosehProcess]): HANDLE;
+      );
+    except
+      on E : EJwsclWinCallFailedException do
+      begin
+        if E.LastError <> 1223 then
+          ShowMessage(e.Message);
+        halt(E.LastError);  
+      end;
+      on E1 : Exception do
+      begin
+        ShowMessage(e1.Message);
+        halt(1);
+      end;
+    end;
+
+  end;
+end.
---
+++ jwscl/trunk/examples/RunAsSys/RunAsSysService.pas
@@ -0,0 +1,220 @@
+unit RunAsSysService;
+
+interface
+
+uses
+  JWaWindows, JwsclToken, JwsclProcess,
+  JwsclTerminalServer, JwsclKnownSid,
+  Messages, SysUtils, Classes, Graphics, Controls, SvcMgr, Dialogs;
+
+type
+  TRunAsSysSvc3 = class(TService)
+    procedure ServiceExecute(Sender: TService);
+    procedure ServiceStart(Sender: TService; var Started: Boolean);
+  private
+    { Private-Deklarationen }
+    procedure OnWinLogonFoundInSameSession(const Sender2 : TJwTerminalServer; var Process : TJwWTSProcess;
+      var Cancel : Boolean; Data : Pointer);
+  public
+    function GetServiceController: TServiceController; override;
+    { Public-Deklarationen }
+    procedure DoExecute;
+  end;
+
+  TServiceApplicationEx = class(TServiceApplication)
+  public
+    procedure RegisterServices(Install, Silent: Boolean);
+  end;
+
+var
+  RunAsSysSvc3 : TRunAsSysSvc3;
+
+implementation
+
+{$R *.DFM}
+
+
+type
+  PInternalProcessData = ^TInternalProcessData;
+  TInternalProcessData = record
+    SessionID : DWORD;
+  end;
+
+procedure TRunAsSysSvc3.OnWinLogonFoundInSameSession(const Sender2 : TJwTerminalServer; var Process : TJwWTSProcess;
+      var Cancel : Boolean; Data : Pointer);
+
+var ProcessData : PInternalProcessData absolute Data;
+begin
+  Cancel :=
+      {
+        same session
+      }
+      (Process.SessionId = ProcessData.SessionID)
+
+      {
+        no idle process
+      }
+      and (Process.ProcessId > 0)
+      {
+        We have to check for system processes in that session.
+        So we ignore them otherwise the user gets a system elevated process.
+      }
+      and (Assigned(Process.UserSid) and Process.UserSid.EqualSid(JwLocalSystemSID));
+end;
+
+procedure ServiceController(CtrlCode: DWord); stdcall;
+begin
+  RunAsSysSvc3.Controller(CtrlCode);
+end;
+
+
+{ TServiceApplicationEx }
+
+procedure TServiceApplicationEx.RegisterServices(Install, Silent: Boolean);
+begin
+  inherited;
+end;
+
+{ TRunAsSysSvc }
+
+procedure TRunAsSysSvc3.DoExecute;
+begin
+  Self.ServiceExecute(nil);
+end;
+
+function TRunAsSysSvc3.GetServiceController: TServiceController;
+begin
+  Result := ServiceController;
+end;
+
+{.$DEFINE CMD}
+procedure TRunAsSysSvc3.ServiceExecute(Sender: TService);
+
+function _ParamCount : Integer;
+begin
+{$IFDEF CMD}
+  result := System.ParamCount;
+{$ELSE}
+  result := Self.ParamCount;
+{$ENDIF}
+end;
+
+function _Param(index : Integer) : String;
+begin
+{$IFDEF CMD}
+  result := System.ParamStr(Index);
+{$ELSE}
+  try
+    result := Self.Param[Index];
+  except
+    result := '';
+  end;
+{$ENDIF}
+end;
+
+var
+  UserToken : TJwSecurityToken;
+
+  iP, i2 : Integer;
+  Parameter : WideString;
+  pParameter : PWideChar;
+
+  StartupInfo : TStartupInfoW;
+  ProcessInfo : TProcessInformation;
+
+  P : Pointer;
+  CmdLine : WideString;
+
+  Data : TInternalProcessData;
+  Meth : TMethod;
+begin
+  //Sleep(10000);
+
+  CmdLine := '';
+  for iP := 0 to _ParamCount  do
+  begin
+    CmdLine := CmdLine + #13#10 + _Param(iP);
+  end;
+  LogMessage(CmdLine,EVENTLOG_INFORMATION_TYPE	,000,000);
+
+  if (_ParamCount < 2) or (
+      ((_ParamCount > 0) and (StrToIntDef(_Param(1),-1) < 0))) then
+  begin
+    Win32ErrCode := ERROR_INVALID_PARAMETER;
+    exit;
+  end;
+
+
+  Data.SessionID := StrToIntDef(_Param(1), -1);
+
+  if Data.SessionID = -1 then
+  begin
+    LogMessage('Given parameter SessionID is invalid',EVENTLOG_ERROR_TYPE);
+    Win32ErrCode := ERROR_INVALID_PARAMETER;
+    exit;
+  end;
+
+  try
+    //UserToken := TJwSecurityToken.CreateWTSQueryUserTokenEx(nil, StrToIntDef(_Param(1),0));
+    UserToken := JwGetTokenFromProcess (OnWinLogonFoundInSameSession, nil, @Data);
+  except
+    on E : Exception do
+    begin
+      LogMessage('Could not get token from user:'+E.Message,EVENTLOG_ERROR_TYPE);
+      Win32ErrCode := ERROR_INVALID_DATA;
+      exit;
+    end;
+  end;
+  try
+    CreateEnvironmentBlock(@P, UserToken.TokenHandle, true);
+
+    //get rest of parameters
+    Parameter := _Param(2);
+
+    if _ParamCount > 2 then
+    begin
+      CmdLine := '';
+      for iP := 3 to _ParamCount-1 do
+      begin
+        CmdLine := CmdLine + ' ' + _Param(iP);
+      end;
+    end;
+
+
+    ZeroMemory(@StartupInfo, sizeof(StartupInfo));
+    StartupInfo.lpDesktop := ('winsta0\default');
+    try
+      if not CreateProcessAsUserW(
+            UserToken.TokenHandle,
+                PWideCHAR(Parameter),
+                PWideCHAR('"'+Parameter+'"'+CmdLine),
+                nil,
+                nil,
+                true,
+                CREATE_NEW_CONSOLE or CREATE_UNICODE_ENVIRONMENT,
+                P,
+                nil,
+                StartupInfo,//__in         LPSTARTUPINFO lpStartupInfo,
+                ProcessInfo //__out        LPPROCESS_INFORMATION lpProcessInformation
+            ) then
+    RaiseLastOSError;
+     except
+      on e: Exception do
+        LogMessage('Could not start process:'+E.Message,EVENTLOG_ERROR_TYPE);
+    end;
+
+    DestroyEnvironmentBlock(P);
+    CloseHandle(ProcessInfo.hProcess);
+    CloseHandle(ProcessInfo.hThread);
+  finally
+    UserToken.Free;
+  end;
+end;
+
+procedure TRunAsSysSvc3.ServiceStart(Sender: TService;
+  var Started: Boolean);
+begin
+  Started := true;
+end;
+
+end.
