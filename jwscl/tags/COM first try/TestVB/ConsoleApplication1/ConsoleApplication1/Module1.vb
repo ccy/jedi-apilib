bd823e82fe9ab733487190fc415f1f5fd955f704
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2008-04-29 17:38:51 +0000

    +com first try
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@409 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

---
+++ jwscl/tags/COM first try/TestVB/ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Private Declare Sub JwOleRaise Lib "..\..\..\..\..\JWSCLCom.dll" _
+     Alias "JwOleRaise" (ByVal Res As Integer)
+
+    Private Declare Function JwHasException Lib "..\..\..\..\..\JWSCLCom.dll" _
+     Alias "JwHasException" (ByVal Res As Integer) As Boolean
+
+
+    Sub Main()
+        JwHasException(0)
+        JwOleRaise(0)
+        Dim fso
+        fso = CreateObject("JWSCLCom.JwCoSid")
+        Try
+            JwOleRaise(fso.InitByName("", "Christian"))
+            Console.Write(fso.StringSid)
+        Catch ex As Exception
+            Console.Write(ex.Message)
+        End Try
+
+        Console.ReadKey()
+
+        fso = Nothing
+    End Sub
+
+End Module
