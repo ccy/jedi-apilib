be1c8a672dfe660214220c384aa0664db9b846cc
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2008-06-01 19:45:20 +0000

    #adapted header for docomatic
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@495 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

--- jwscl/trunk/source/JwsclAccounts.pas
+++ jwscl/trunk/source/JwsclAccounts.pas
@@ -1,40 +1,45 @@
-{<B>Abstract</B>Contains Windows account control classes - Not implemented yet 
-@author(Christian Wimmer)
-<B>Created:</B>03/23/2007 
-<B>Last modification:</B>09/10/2007 
-
-Project JEDI Windows Security Code Library (JWSCL)
-
-The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy of the
-License at http://www.mozilla.org/MPL/
-
-Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
-ANY KIND, either express or implied. See the License for the specific language governing rights
-and limitations under the License.
-
-Alternatively, the contents of this file may be used under the terms of the  
-GNU Lesser General Public License (the  "LGPL License"), in which case the   
-provisions of the LGPL License are applicable instead of those above.        
-If you wish to allow use of your version of this file only under the terms   
-of the LGPL License and not to allow others to use your version of this file 
-under the MPL, indicate your decision by deleting  the provisions above and  
-replace  them with the notice and other provisions required by the LGPL      
-License.  If you do not delete the provisions above, a recipient may use     
-your version of this file under either the MPL or the LGPL License.          
-                                                                             
-For more information about the LGPL: http://www.gnu.org/copyleft/lesser.html 
-
-The Original Code is JwsclAccounts.pas.
-
-The Initial Developer of the Original Code is Christian Wimmer.
-Portions created by Christian Wimmer are Copyright (C) Christian Wimmer. All rights reserved.
-
-
-Description:
-Not implemented yet
-
-
+{ Description
+  Project JEDI Windows Security Code Library (JWSCL)
+  
+  Contains Windows account control classes - Not implemented
+  yet
+  
+  <b>Not implemented yet</b>
+  
+  Author
+  Christian Wimmer
+  
+  License
+  The contents of this file are subject to the Mozilla Public 
+  License Version 1.1 (the "License"); you may not use this
+  file except in compliance with the License. You may obtain a
+  copy of the License at http://www.mozilla.org/MPL
+  
+  Software distributed under the License is distributed on an
+  "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express
+  or implied. See the License for the specific language
+  governing rights and limitations under the License.
+  
+  Alternatively, the contents of this file may be used under
+  the terms of the GNU Lesser General Public License (the "LGPL
+  \License"), in which case the provisions of the LGPL License
+  are applicable instead of those above. If you wish to allow
+  use of your version of this file only under the terms of the
+  LGPL License and not to allow others to use your version of
+  this file under the MPL, indicate your decision by deleting
+  the provisions above and replace them with the notice and
+  other provisions required by the LGPL License. If you do not
+  delete the provisions above, a recipient may use your version
+  of this file under either the MPL or the LGPL License.
+  
+  For more information about the LGPL:
+  http://www.gnu.org/copyleft/lesser.html
+  
+  The Original Code is JwsclAccounts.pas.
+  
+  The Initial Developer of the Original Code is Christian
+  Wimmer. Portions created by Christian Wimmer are Copyright
+  (C) Christian Wimmer. All rights reserved.                    
 }
 {$IFNDEF SL_OMIT_SECTIONS}
 unit JwsclAccounts;
