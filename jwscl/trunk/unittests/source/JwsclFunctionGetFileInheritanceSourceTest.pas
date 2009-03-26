ec76689ea8b613d9a1b04acaec31633ec219644b
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2009-03-26 11:29:12 +0000

    #syntax fix
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@771 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

--- jwscl/trunk/unittests/source/JwsclFunctionGetFileInheritanceSourceTest.pas
+++ jwscl/trunk/unittests/source/JwsclFunctionGetFileInheritanceSourceTest.pas
@@ -37,7 +37,7 @@ type
     procedure CheckInheritance(
        const ExcpetedInheritance : TJwInheritedFromArray;
        const Inheritance : TJwInheritedFromArray;
-       const Msg : String = '');
+       Msg : String = '');
 
     procedure SetUp; override;
     procedure TearDown; override;
@@ -48,7 +48,14 @@ type
   published
     procedure Test_1;
     procedure Test_2;
+    procedure Test_3;
+    procedure Test_4;
+    procedure Test_5;
+    procedure Test_6;
   end;
+
+
+
 
   TAccessEntry = class;
 
@@ -112,14 +119,43 @@ end;
 procedure TestTTestGetFileInheritanceFunction.CheckInheritance(
   const ExcpetedInheritance : TJwInheritedFromArray;
   const Inheritance: TJwInheritedFromArray;
-  const Msg: String);
-var i : Integer;
+  Msg: String);
+var
+  i, i2 : Integer;
+  b : Boolean;
+  In1, In2 : TJwInheritedFromRecord;
 begin
   CheckEquals(Length(ExcpetedInheritance), Length(Inheritance), 'Number of inheritance entries differs' +#13#10 + msg);
 
   for I := 0 to Length(Inheritance) - 1 do
   begin
-    Check(CompareMem(@Inheritance[i], @ExcpetedInheritance[i], sizeof(TJwInheritedFromRecord)),
+    In1 := Inheritance[i];
+    In2 := ExcpetedInheritance[i];
+
+    B := (In1.GenerationGap = In2.GenerationGap) and
+      (In1.AncestorName = In2.AncestorName) and
+      (In1.SID = In2.SID) and
+      (In1.SIDString = In2.SIDString) and
+      (In1.System = In2.System);
+    if not B then
+    begin
+      for I2 := 0 to Length(Inheritance) - 1 do
+      begin
+        Msg := Msg  + Format('\nIndex: %d\nGenerationGap: %d (%d)\nAncestorName: %s (%s)\n'+
+          'SID: %s (%s)\nUserName: %s (%s)\nSystem: %s (%s)\n\n',
+        [i2,
+        Inheritance[i2].GenerationGap, ExcpetedInheritance[i2].GenerationGap,
+        Inheritance[i2].AncestorName, ExcpetedInheritance[i2].AncestorName,
+        Inheritance[i2].SID, ExcpetedInheritance[i2].SID,
+        Inheritance[i2].UserName, ExcpetedInheritance[i2].UserName,
+        Inheritance[i2].System, ExcpetedInheritance[i2].System
+        ]);
+      end;
+
+      JwReplaceBreaks(Msg);
+    end;
+
+    Check(B,
       Format('Inheritance entry %d failed.'#13#10,[i])+ Msg);
   end;
 end;
@@ -146,7 +182,7 @@ begin
   AssignAndFree(InheritancePath.SD['C:\']);
 
   CheckInheritance(
-    TInheritanceEntry.Create.
+    TInheritanceEntry.Init.
       Add('', 0, nil).
     Get(),
     FJwSecureFileObject.GetFileInheritanceSource(fMainPath, true,
@@ -157,7 +193,65 @@ end;
 procedure TestTTestGetFileInheritanceFunction.Test_2;
 begin
   //Check(false, 'Not implemented');
-  fMainPath := 'C:\Docs\';
+  fMainPath := 'C:\Docs\Test';
+  fInheritancePath := TJwInheritancePath.Create(fMainPath);
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, []).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afObjectInheritAce]).
+  AssignAndFree(InheritancePath.SD['C:\']);
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afContainerInheritAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce,afObjectInheritAce]).
+  AssignAndFree(InheritancePath.SD['C:\Docs\']);
+
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afContainerInheritAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afObjectInheritAce]).
+  AssignAndFree(InheritancePath.SD['C:\Docs\Test\']);
+
+
+  CheckInheritance(
+
+    TInheritanceEntry.Init.
+      Add('',0, nil).
+      Add('',0, nil).
+    Get(),
+
+    FJwSecureFileObject.GetFileInheritanceSource('C:\', true,
+        [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
+    'C:\ failed');
+
+  CheckInheritance(
+
+    TInheritanceEntry.Init.
+      Add('',0, nil).
+      Add('C:\',1, JwAdministratorsSID).
+    Get(),
+
+    FJwSecureFileObject.GetFileInheritanceSource('C:\Docs', true,
+        [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
+    'C:\Docs failed');
+
+  CheckInheritance(
+
+    TInheritanceEntry.Init.
+      Add('C:\Docs\',1, JwAdministratorsSID).
+      Add('C:\',2, JwAdministratorsSID).
+    Get(),
+
+    FJwSecureFileObject.GetFileInheritanceSource(fMainPath, true,
+        [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
+    'C:\Docs\Test\');
+end;
+
+
+procedure TestTTestGetFileInheritanceFunction.Test_3;
+begin
+  //Check(false, 'Not implemented');
+  fMainPath := 'C:\Docs\Test';
   fInheritancePath := TJwInheritancePath.Create(fMainPath);
 
   TAccessList.CreateSecurity.
@@ -170,17 +264,145 @@ begin
     AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce,afObjectInheritAce]).
   AssignAndFree(InheritancePath.SD['C:\Docs\']);
 
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afContainerInheritAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afObjectInheritAce]).
+  AssignAndFree(InheritancePath.SD['C:\Docs\Test\']);
+
+
   CheckInheritance(
 
-    TInheritanceEntry.Create.
-      Add('',1, JwAdministratorsSID).
+    TInheritanceEntry.Init.
+      Add('C:\',1, JwAdministratorsSID).
+      Add('C:\',1, JwAdministratorsSID).
+    Get(),
+
+    FJwSecureFileObject.GetFileInheritanceSource('C:\Docs\', true,
+        [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
+    'C:\Docs\ failed');
+
+  CheckInheritance(
+
+    TInheritanceEntry.Init.
+      Add('C:\',2, JwAdministratorsSID).
+      Add('C:\',2, JwAdministratorsSID).
     Get(),
 
     FJwSecureFileObject.GetFileInheritanceSource(fMainPath, true,
         [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
-    '');
+    'C:\Docs\Test\ failed');
 end;
 
+procedure TestTTestGetFileInheritanceFunction.Test_4;
+begin
+  //Check(false, 'Not implemented');
+  fMainPath := 'C:\Docs\Test';
+  fInheritancePath := TJwInheritancePath.Create(fMainPath);
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afContainerInheritAce, afObjectInheritAce, afInheritOnlyAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, []).
+
+  AssignAndFree(InheritancePath.SD['C:\']);
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afContainerInheritAce, afObjectInheritAce, afInheritOnlyAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce]).
+  AssignAndFree(InheritancePath.SD['C:\Docs\']);
+
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afContainerInheritAce, afObjectInheritAce, afInheritOnlyAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce]).
+  AssignAndFree(InheritancePath.SD['C:\Docs\Test\']);
+
+  CheckInheritance(
+
+    TInheritanceEntry.Init.
+      Add('C:\',1, JwAdministratorsSID).
+      Add('C:\',1, JwAdministratorsSID).
+    Get(),
+
+    FJwSecureFileObject.GetFileInheritanceSource('C:\Docs\', true,
+        [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
+    'C:\Docs\ failed');
+
+  CheckInheritance(
+
+    TInheritanceEntry.Init.
+      Add('C:\',2, JwAdministratorsSID).
+      Add('C:\',2, JwAdministratorsSID).
+    Get(),
+
+    FJwSecureFileObject.GetFileInheritanceSource(fMainPath, true,
+        [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
+    'C:\Docs\Test\ failed');
+end;
+
+procedure TestTTestGetFileInheritanceFunction.Test_5;
+begin
+  //Check(false, 'Not implemented');
+  fMainPath := 'C:\Docs\Test';
+  fInheritancePath := TJwInheritancePath.Create(fMainPath);
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afContainerInheritAce, afObjectInheritAce, afInheritOnlyAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, []).
+
+  AssignAndFree(InheritancePath.SD['C:\']);
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afContainerInheritAce, afObjectInheritAce, afInheritOnlyAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce]).
+  AssignAndFree(InheritancePath.SD['C:\Docs\']);
+
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afContainerInheritAce, afObjectInheritAce, afInheritOnlyAce]).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce]).
+  AssignAndFree(InheritancePath.SD['C:\Docs\Test\']);
+
+  CheckInheritance(
+
+    TInheritanceEntry.Init.
+      Add('C:\',2, JwAdministratorsSID).
+      Add('C:\',2, JwAdministratorsSID).
+    Get(),
+
+    FJwSecureFileObject.GetFileInheritanceSource(fMainPath, true,
+        [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
+    'C:\Docs\Test\ failed');
+end;
+
+procedure TestTTestGetFileInheritanceFunction.Test_6;
+begin
+  //Check(false, 'Not implemented');
+  fMainPath := 'C:\Docs\';
+  fInheritancePath := TJwInheritancePath.Create(fMainPath);
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, []).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afContainerInheritAce, afObjectInheritAce]).
+  AssignAndFree(InheritancePath.SD['C:\']);
+
+  TAccessList.CreateSecurity.
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, []).
+    AddAllow(GENERIC_ALL, JwAdministratorsSID, [afInheritedAce, afContainerInheritAce, afObjectInheritAce]).
+  AssignAndFree(InheritancePath.SD['C:\Docs\']);
+
+
+  CheckInheritance(
+
+    TInheritanceEntry.Init.
+      Add('',0, nil).
+      Add('C:\',1, JwAdministratorsSID).
+    Get(),
+
+    FJwSecureFileObject.GetFileInheritanceSource(fMainPath, true,
+        [siDaclSecurityInformation], OnGetNamedSecurityInfo, nil),
+    'C:\Docs\ failed');
+end;
 
 { TAccessList }
 
@@ -252,7 +474,7 @@ begin
   if Length(Arr) = 0 then
     SetLength(Arr, 1)
   else
-    SetLength(Arr, High(Arr)+1);
+    SetLength(Arr, Length(Arr)+1);
 
   i := High(Arr);
   ZeroMemory(@Arr[i], sizeof(Arr[i]));
