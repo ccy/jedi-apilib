7bb1d63c7a8b34ab06f45cd89f5b100f47a26e6f
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2010-07-28 15:18:26 +0000

    #updated docs #fixed a bug with CheckTokenMembership
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@992 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

--- jwscl/branches/0.9.3/source/JwsclToken.pas
+++ jwscl/branches/0.9.3/source/JwsclToken.pas
@@ -855,7 +855,11 @@ type
 
     {<B>CheckTokenMembership</B> checks whether a given SID is member of the token.
      It returns true if the SID could be found in the list ignoring
-     whether the SID is enabled or not; otherwise it returns false.}
+     whether the SID is enabled or not; otherwise it returns false.
+
+     Exceptions
+      EJwsclWinCallFailedException This exception is thrown if winapi call CheckTokenMembership failed.
+    }
     function CheckTokenMembership(aSidToCheck: TJwSecurityId): boolean;
 
         {<B>IsEqual</B> compares the token instance with a second one.
@@ -4734,7 +4738,12 @@ begin
   CheckTokenHandle('CheckTokenMembership');
 
   bRes := True;
-  jwaWindows.CheckTokenMembership(Self.TokenHandle, @aSidToCheck.SID, bRes);
+  if not jwaWindows.CheckTokenMembership(Self.TokenHandle, aSidToCheck.SID,
+    bRes) then
+    raise EJwsclWinCallFailedException.CreateFmtEx(
+      RsWinCallFailed, 'CheckTokenMembership',
+      ClassName, RsUNToken, 0, True, ['CheckTokenMembership']);
+
   Result := bRes;
 end;
 
