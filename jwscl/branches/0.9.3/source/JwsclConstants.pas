36c6dbfa9a65f6caf6c3fc055d8ed9edc885658d
dezipaitor <dezipaitor@b33855ce-ec3c-0410-bd6a-a8d1cd62a336>
2010-10-20 17:36:54 +0000

    #fixed value of SM_SERVERR2
    git-svn-id: http://svn.code.sf.net/p/jedi-apilib/code@1033 b33855ce-ec3c-0410-bd6a-a8d1cd62a336

--- jwscl/branches/0.9.3/source/JwsclConstants.pas
+++ jwscl/branches/0.9.3/source/JwsclConstants.pas
@@ -110,7 +110,7 @@ const
     {<B>SM_SERVERR2</B> is used as an additional constant for GetSystemMetrics
      to detect the second release of Win2003
     }
-  SM_SERVERR2 = 98;
+  SM_SERVERR2 = 89;
 
 
   { operating system (OS)constants }
